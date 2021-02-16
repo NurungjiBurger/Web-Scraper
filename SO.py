bb0f2357bd8c156a0d7a0027c11fcf88a9dc4ab2
NurungjiBurger <qazw181900@naver.com>
2021-02-16 02:23:24 +0000

    add extracting information in stackoverflow

---
+++ SO.py
@@ -0,0 +1,44 @@
+# 페이지를 가져오고
+# request 를 만들고
+# 원하는 정보를 추출
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
+
+
+def get_last_page():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+  last_page = pages[-2].get_text(strip=True)
+  return int(last_page)
+
+def extract_job(html):
+  title = html.find("h2", {"class":"mb4"}).find("a")["title"]
+  # print(title)
+  company, location = html.find("h3", {"class": "fc-black-700"}).find_all("span",recursive=False)
+  # recursive=False 로 첫단계의 span 들만 가져옴
+  company = company.get_text(strip=True).strip("-")
+  location = location.get_text(strip=True)
+  print(company, location)
+  return {'title': title, 'company': company, 'location': location}   
+
+def extract_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page+1}")
+    # print(result.status)
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"-job"})
+    for result in results:
+      # print(result["data-jobid"])
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  return jobs
--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_pages():
   result = requests.get(URL)
 
   soup = BeautifulSoup(result.text, "html.parser")
@@ -28,17 +28,20 @@ def extract_job(html):
   company = html.find("span", {"class": "company"})
   company_anchor = company.find("a")
   # delete None 1
-  if company_anchor is not None:
-    company = (str(company_anchor.string))
+  if company:
+    if company_anchor is not None:
+      company = (str(company_anchor.string))
+    else:
+      company = (str(company.string))
+    company = company.strip()
   else:
-    company = (str(company.string))
-  company = company.strip()
+    company = None
   # delete None 2
   location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
   job_id = html["data-jk"]
   return {'title': title, 'company': company, 'location': location, "link": f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
     print(f"Scrapping page {page}")
@@ -50,3 +53,9 @@ def extract_indeed_jobs(last_page):
       jobs.append(job)
   return jobs
 
+def get_jobs():
+  last_pages = get_last_pages()
+
+  jobs = extract_jobs(last_pages)
+
+  return jobs
